b1ffd1535d340bcdfc09aec6e96a213d6b59d796
aokisou <kd1380646@st.kobedenshi.ac.jp>
2024-05-21 09:49:42 +0900

    船を出した
    モデルを作って船を出した

---
+++ 004_サイドビュー/Src/Application/Object/Stage/Stage.cpp
@@ -0,0 +1,12 @@
+﻿#include "Stage.h"
+
+void Stage::Init()
+{
+	m_model.Load("Asset/Models/SideViewMap/SideViewMap.gltf");
+	m_mWorld = Math::Matrix::CreateTranslation(0, 0, 10);
+}
+
+void Stage::DrawLit()
+{
+	KdShaderManager::Instance().m_StandardShader.DrawModel(m_model,m_mWorld);
+}
---
+++ 004_サイドビュー/Src/Application/Scene/GameScene/GameScene.cpp
@@ -0,0 +1,31 @@
+﻿#include "GameScene.h"
+#include"../SceneManager.h"
+#include "../../Object/Stage/Stage.h"
+
+void GameScene::Event()
+{
+	if (GetAsyncKeyState('T') & 0x8000)
+	{
+		SceneManager::Instance().SetNextScene
+		(
+			SceneManager::SceneType::Title
+		);
+	}
+
+	Math::Matrix transMat, rotateXMat, rotateYMat, cameraMat;
+	transMat = Math::Matrix::CreateTranslation(0, 5, -5);
+	rotateXMat = Math::Matrix::CreateRotationX(DirectX::XMConvertToRadians(0));
+	rotateYMat = Math::Matrix::CreateRotationY(DirectX::XMConvertToRadians(0));
+	cameraMat = rotateXMat * transMat * rotateYMat;
+
+	m_camera->SetCameraMatrix(cameraMat);
+}
+
+void GameScene::Init()
+{
+	m_camera = std::make_unique<KdCamera>();
+
+	std::shared_ptr<Stage> obj;
+	obj = std::make_shared<Stage>();
+	AddObject(obj);
+}
