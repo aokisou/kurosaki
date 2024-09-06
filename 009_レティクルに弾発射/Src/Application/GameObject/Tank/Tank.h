ee4399b76a975a34955e7b461acb805ee01cd228
aokisou <kd1380646@st.kobedenshi.ac.jp>
2024-09-07 00:18:36 +0900

    09/07

---
+++ 009_レティクルに弾発射/Src/Application/GameObject/Tank/Tank.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+class CameraBase;
+
+class Tank :public KdGameObject
+{
+public:
+	Tank() { Init(); }
+	~Tank()override{}
+
+	void Init()override;
+	void Update()override;
+	void PostUpdate()override;
+	void DrawLit()override;
+	void DrawSprite()override;
+
+	void SetCamera(const std::shared_ptr<CameraBase>& _spCamera) { m_wpCamera = _spCamera; }
+private:
+	Math::Vector3 m_pos = {};
+	Math::Vector3 m_moveDir = {};
+
+	Math::Vector3 m_angle = {};
+
+	std::shared_ptr<KdModelData> m_spModel = {};
+	std::weak_ptr<CameraBase> m_wpCamera;
+	std::shared_ptr<KdTexture> m_spRetTex;
+
+	bool m_bTPS = false;
+};
---
+++ 009_レティクルに弾発射/Src/Application/Scene/GameScene/GameScene.cpp
@@ -0,0 +1,42 @@
+﻿#include "GameScene.h"
+#include"../SceneManager.h"
+#include "../../GameObject/Camera/TPSCamera/TPSCamera.h"
+#include "../../GameObject/Ground/Ground.h"
+#include "../../GameObject/Tank/Tank.h"
+#include "../../GameObject/Enemy/Moto/Moto.h"
+#include "../../GameObject/Enemy/Yama/Yama.h"
+#include "../../GameObject/Bullet/Bullet.h"
+#include "../../main.h"
+
+void GameScene::Event()
+{
+	Application::Instance().m_log.Clear();
+	Application::Instance().m_log.AddLog("objList : %d\n", (int)m_objList.size());
+}
+
+void GameScene::Init()
+{
+	std::shared_ptr<Ground> ground = std::make_shared<Ground>();
+	AddObject(ground);
+
+	std::shared_ptr<Moto> m = std::make_shared<Moto>();
+	m->SetPos({ 10,0,10 });
+	AddObject(m);
+	std::shared_ptr<Yama> y = std::make_shared<Yama>();
+	y->SetPos({ -10,0,10 });
+	AddObject(y);
+
+	std::shared_ptr<Tank> tank = std::make_shared<Tank>();
+	m->SetTarget(tank);
+	y->SetTarget(tank);
+	AddObject(tank);
+
+	//std::shared_ptr<Bullet> b = std::make_shared<Bullet>();
+	//AddObject(b);
+
+	std::shared_ptr<TPSCamera> camera = std::make_shared<TPSCamera>();
+	camera->Init();
+	tank->SetCamera(camera);
+	camera->SetTarget(tank);
+	AddObject(camera);
+}
