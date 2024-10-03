4e6ad4a9da06b962bf3735dfc11476c74e30f027
aokisou <kd1380646@st.kobedenshi.ac.jp>
2024-10-03 23:33:34 +0900

    10/03

---
+++ 014_コーンライト/Src/Application/GameObject/Ground/Ground.cpp
@@ -0,0 +1,22 @@
+﻿#include "Ground.h"
+
+void Ground::Init()
+{
+	m_spModel = std::make_shared<KdModelData>();
+	m_spModel->Load("Asset/Models/Ground/Ground.gltf");
+
+	Math::Matrix scaleMat,transMat;
+	scaleMat = Math::Matrix::CreateScale(100);
+	transMat = Math::Matrix::CreateTranslation(0,0,0);
+	m_mWorld = scaleMat * transMat;
+}
+
+void Ground::DrawLit()
+{
+	if (m_spModel)
+	{
+		KdShaderManager::Instance().m_StandardShader.DrawModel(*m_spModel, m_mWorld);
+		Math::Matrix mat = m_mWorld * Math::Matrix::CreateRotationX(DirectX::XMConvertToRadians(90));
+		KdShaderManager::Instance().m_StandardShader.DrawModel(*m_spModel, mat);
+	}
+}
---
+++ 014_コーンライト/Src/Application/Scene/GameScene/GameScene.cpp
@@ -0,0 +1,27 @@
+﻿#include "GameScene.h"
+#include"../SceneManager.h"
+#include "../../GameObject/Camera/TPSCamera/TPSCamera.h"
+#include "../../GameObject/Ground/Ground.h"
+#include "../../GameObject/Tank/Tank.h"
+
+void GameScene::Event()
+{
+}
+
+void GameScene::Init()
+{
+	KdShaderManager::Instance().WorkAmbientController().SetDirLight(Math::Vector3(1, -1, 1), Math::Vector3(0, 0, 0));
+	KdShaderManager::Instance().WorkAmbientController().SetAmbientLight(Math::Vector4(0.1, 0.1, 0.1, 1));
+
+	std::shared_ptr<Tank> tank = std::make_shared<Tank>();
+	AddObject(tank);
+
+	std::shared_ptr<Ground> ground = std::make_shared<Ground>();
+	AddObject(ground);
+
+	std::shared_ptr<TPSCamera> camera = std::make_shared<TPSCamera>();
+	camera->Init();
+	tank->SetCamera(camera);
+	camera->SetTarget(tank);
+	AddObject(camera);
+}
